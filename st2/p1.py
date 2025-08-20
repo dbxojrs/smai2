dec37a0e2c837718af615834d27f0b032b8d18ed
유태건 <dbxorjs7@naver.com>
2025-08-20 13:18:20 +0900

    0820

--- st2/p1.py
+++ st2/p1.py
@@ -1,3 +1,22 @@
 import streamlit as st
+from numpy.distutils.system_info import language_map
+from streamlit import markdown
 
+st.sidebar.markdown("clicked page1")
+st.sidebar.markdown("Side Menu")
+st.sidebar.button("Click")
+st.sidebar.radio("성별", ["여자","남자"])
+#page
 st.title("page1")
+st.markdown("Mark Down")
+st.header("header")
+st.subheader("subheader")
+st.caption("caption")
+st.code("""
+ def func():
+ print("OK")
+ def func2():
+ print("OK2")
+ 
+ 
+ """,language="python")
---
+++ st2/p7.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+
+#side bar
+st.sidebar.markdown("clicked page7")
+#page
+st.title("page7")
